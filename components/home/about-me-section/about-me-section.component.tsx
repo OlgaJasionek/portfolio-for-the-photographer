50a0755eab3610e3156d5c3a81966e6a5d1723c8
olga.jasionek <ogi22021996@gmail.com>
2023-05-08 10:12:31 +0200

    about me animation

--- components/home/about-me-section/about-me-section.component.tsx
+++ components/home/about-me-section/about-me-section.component.tsx
@@ -1,46 +1,59 @@
 import classnames from "classnames";
 import { useRouter } from "next/router";
+import { motion } from "framer-motion";
 
 import Button from "@/common/components/button/button.component";
 import Marker from "@/common/components/marker/marker.component";
 
 import styles from "./about-me-section.module.scss";
+import { useRef } from "react";
 
 const AboutMeSection = () => {
   const router = useRouter();
+  const scrollRef = useRef(null);
 
   return (
     <div className={styles.wrapper}>
       <div className='container'>
-        <div className={styles.content}>
-          <div className={styles.text}>
-            <Marker text='O MNIE' />
-            <h1 className={classnames(styles.title, "text-xl")}>
-              WITAJ W MOIM ŚWIECIE!
-            </h1>
-            <div className={styles.description}>
-              <p>
-                Mam na imię Natalia, aparatem chwytam chwile, które budują moje
-                wspomnienia. Tym chciałabym podzielić się z Wami.
-              </p>
+        <div ref={scrollRef}>
+          <motion.div
+            className={styles.content}
+            initial={{ y: 400, opacity: 0 }}
+            whileInView={{
+              y: 0,
+              opacity: 1,
+              transition: { duration: 1, bounce: 0.1, type: "spring" },
+            }}
+            viewport={{ root: scrollRef }}>
+            <div className={styles.text}>
+              <Marker text='O MNIE' />
+              <h1 className={classnames(styles.title, "text-xl")}>
+                WITAJ W MOIM ŚWIECIE!
+              </h1>
+              <div className={styles.description}>
+                <p>
+                  Mam na imię Natalia, aparatem chwytam chwile, które budują
+                  moje wspomnienia. Tym chciałabym podzielić się z Wami.
+                </p>
+              </div>
+              <div className={styles.description}>
+                <p>
+                  Pozwólcie mi uwiecznić najpiękniejsze momenty Waszego życia,
+                  dajcie mi możliwość aby utrwalić je dla Was i zachować w
+                  pięknych kadrach, abyście mogli się nimi dzielić i przeżywać
+                  jeszcze raz.
+                </p>
+              </div>
+              <Button
+                theme='contained'
+                onClick={() => {
+                  router.push("/o-mnie");
+                }}>
+                POZNAJ MNIE LEPIEJ
+              </Button>
             </div>
-            <div className={styles.description}>
-              <p>
-                Pozwólcie mi uwiecznić najpiękniejsze momenty Waszego życia,
-                dajcie mi możliwość aby utrwalić je dla Was i zachować w
-                pięknych kadrach, abyście mogli się nimi dzielić i przeżywać
-                jeszcze raz.
-              </p>
-            </div>
-            <Button
-              theme='contained'
-              onClick={() => {
-                router.push("/o-mnie");
-              }}>
-              POZNAJ MNIE LEPIEJ
-            </Button>
-          </div>
-          <div className={styles.img}></div>
+            <div className={styles.img}></div>
+          </motion.div>
         </div>
       </div>
     </div>
